a310d6c0b2a55b76643954d78dd75540c6a526e6
kyle jacobs <kjacobs888@gmail.com>
2017-08-09 13:04:08 -0400

    cssfolder

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-// function myFunction() {
-// var = greeting;
-// var time = new Date().getHours();
-// if (time < 10)
-// {
-//     greeting = "Good morning";
-// } else if (time < 20) {
-//     greeting = "Good afternoon";
-// } else {
-//     greeting = "Good evening";}
-//     document.getElementById("demo").innerHTML = greeting;
-//   }
+function myFunction() {
+var  greeting;
+var time = new Date().getHours();
+if (time < 10)
+{
+    greeting = "Good morning";
+} else if (time < 20) {
+    greeting = "Good afternoon";
+} else {
+    greeting = "Good evening";}
+    document.getElementById("demo").innerHTML = greeting;
+  }
 // $(document).ready(function(){
 //     $("button").click(function(){
 //         $("div").animate({left: '250px'});
 //     });
 // });
-$(document).ready(function(){
-    $("button").click(function(){
-        $("#flex-item1").fadeIn();
-        $("#flex-item2").fadeIn("slow");
-        $("#flex-item3").fadeIn(3000);
-    });
-});
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $("#flex-item1").fadeIn();
+//         $("#flex-item2").fadeIn("slow");
+//         $("#flex-item3").fadeIn(3000);
+//     });
+// });
